30e7fd9b4f633a6dfdd025c0d3002f1f7854a58b
abrukman <brukmanster@gmail.com>
2026-05-21 15:34:22 -0300

    se implementa validacion con regex

--- app.js
+++ app.js
@@ -19,7 +19,20 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
+
+
 function configurarPapelito(papelito) {
+    const contenedorPapelito = document.createElement("div");
+    contenedorPapelito.classList.add("contenedor-papelito");
+    contenedorPapelito.appendChild(papelito);
+    container.appendChild(contenedorPapelito);
+
+    const mensajeError = document.createElement("div");
+    mensajeError.classList.add("toast-error");
+    mensajeError.classList.add("oculto");
+    mensajeError.textContent = "El nombre solo puede contener letras y/o espacios";
+    papelito.after(mensajeError);
+
     papelito.id = "papelito";
     papelito.classList.add("estilo-papelito");
     papelito.draggable = true;
@@ -28,6 +41,15 @@ function configurarPapelito(papelito) {
         papelito.contentEditable = true;
         papelito.focus();
         papelito.draggable = false;
+    });
+
+    papelito.addEventListener("input", () => {
+        let nombreIngresado = papelito.textContent;
+        if (!reglaNombre.test(nombreIngresado)) {
+            mensajeError.classList.remove("oculto");
+        } else {
+            mensajeError.classList.add("oculto");
+        };
     });
 
     papelito.addEventListener('keydown', (evento) => {
@@ -43,12 +65,20 @@ function configurarPapelito(papelito) {
     });
 
     papelito.addEventListener("dragstart", (evento) => {
+        if (!reglaNombre.test(papelito.textContent)) {
+            evento.preventDefault();
+            return;
+        }
+
+        evento.dataTransfer.effectAllowed = "move";
+        evento.dataTransfer.dropEffect = "move";
         evento.dataTransfer.setData("text/plain", evento.target.id);
     });
 }
 
 const container = document.getElementById("container");
 const papelito = document.getElementById("papelito");
+const reglaNombre = /^[a-zA-Z áéíóúñÑÁÉÍÓÚüÜ]+$/;
 
 configurarPapelito(papelito);
 
@@ -73,6 +103,11 @@ bolsa.addEventListener("drop", async (event) => {
     const idPapelito = event.dataTransfer.getData("text/plain");
     const papelito = document.getElementById(idPapelito);
     const nombreNormalizado = papelito.textContent.toLowerCase().trim();
+
+    if(!reglaNombre.test(nombreNormalizado)) {
+        alert(`El nombre ${nombreNormalizado} no es un nombre válido para participar del sorteo. Los nombres solo pueden tener letras y/o espacios`);
+        return;
+    }
 
     const q = query(collection(db, "nombres"), where("nombre", "==", nombreNormalizado));
     const consulta = await getDocs(q);
