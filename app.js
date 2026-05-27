d8faccc842fd08c78d4bc7770620153a798e5680
abrukman <brukmanster@gmail.com>
2026-05-27 17:09:14 -0300

    se agrega css y se refina la normalización para no repetir nombres con acentos pero mantener la ñ a salvo

--- app.js
+++ app.js
@@ -24,8 +24,27 @@ const db = getFirestore(app);
 function configurarPapelito(papelito) {
     const contenedorPapelito = document.createElement("div");
     contenedorPapelito.classList.add("contenedor-papelito");
+
+    //Logica aspecto realista
+    const texturas = ["pattern-liso", "pattern-rayado", "pattern-cuadriculado"];
+    const formas = ["shape-rasgado-1", "shape-rasgado-2", "shape-rasgado-3"];
+
+    const texturaAlAzar = texturas[Math.floor(Math.random() * texturas.length)];
+    const formaAlAzar = formas[Math.floor(Math.random() * formas.length)];
+
+    papelito.classList.add(texturaAlAzar);
+    papelito.classList.add(formaAlAzar);
+
+    const giroAlAzar = (Math.random() * 6 - 3).toFixed(2);
+    papelito.style.transform = `rotate(${giroAlAzar}deg)`;
+
     contenedorPapelito.appendChild(papelito);
-    container.appendChild(contenedorPapelito);
+    if (typeof container !== 'undefined') {
+        container.appendChild(contenedorPapelito);
+    } else {
+        document.getElementById("container").appendChild(contenedorPapelito);
+    }
+    
 
     const mensajeError = document.createElement("div");
     mensajeError.classList.add("toast-error");
@@ -45,10 +64,18 @@ function configurarPapelito(papelito) {
 
     papelito.addEventListener("input", () => {
         let nombreIngresado = papelito.textContent;
-        if (!reglaNombre.test(nombreIngresado)) {
+        if (nombreIngresado === "") {
+            mensajeError.classList.add("oculto");
+            papelito.draggable = false;
+            papelito.style.cursor = "not-allowed";
+        } else if (!reglaNombre.test(nombreIngresado)) {
             mensajeError.classList.remove("oculto");
+            papelito.draggable = false;
+            papelito.style.cursor = "not-allowed";
         } else {
             mensajeError.classList.add("oculto");
+            papelito.draggable =  true;
+            papelito.style.cursor = "grab";
         };
     });
 
@@ -76,6 +103,16 @@ function configurarPapelito(papelito) {
     });
 }
 
+function limpiarAcentos(texto) {
+    return texto
+    .replace(/ñ/g, "##enye_min##")
+    .replace(/Ñ/g, "##enye_may##")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/##enye_min##/, "ñ")
+    .replace(/##enye_may##/, "Ñ");
+}
+
 const container = document.getElementById("container");
 const papelito = document.getElementById("papelito");
 const reglaNombre = /^[a-zA-Z áéíóúñÑÁÉÍÓÚüÜ]+$/;
@@ -88,7 +125,6 @@ const btnNuevoPapelito = document.getElementById("nuevo-papelito");
 btnNuevoPapelito.addEventListener("click", () => {
     const nuevoPapelito = document.createElement("div");
     configurarPapelito(nuevoPapelito);
-    container.appendChild(nuevoPapelito);
     btnNuevoPapelito.style.display = "none";
 });
 
@@ -98,11 +134,21 @@ bolsa.addEventListener("dragover", (event) => {
     event.preventDefault();
 });
 
+bolsa.addEventListener("dragenter", (event) => {
+    event.preventDefault();
+    bolsa.classList.add("bolsa-activa");
+});
+
+bolsa.addEventListener("dragleave", (event) => {
+    bolsa.classList.remove("bolsa-activa");
+})
+
 bolsa.addEventListener("drop", async (event) => {
     event.preventDefault();
+    bolsa.classList.remove("bolsa-activa");
     const idPapelito = event.dataTransfer.getData("text/plain");
     const papelito = document.getElementById(idPapelito);
-    const nombreNormalizado = papelito.textContent.toLowerCase().trim();
+    const nombreNormalizado = limpiarAcentos(papelito.textContent.toLowerCase().trim());
 
     if(!reglaNombre.test(nombreNormalizado)) {
         alert(`El nombre ${nombreNormalizado} no es un nombre válido para participar del sorteo. Los nombres solo pueden tener letras y/o espacios`);
